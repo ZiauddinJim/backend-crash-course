72978c7f2c1081ca8ee76c08dc9cfa3c157c97fd
ziauddinJim <ziauddinjim@outlook.com>
2026-05-13 08:48:11 +0600

    Initialize backend project with TypeScript, Express, and basic server setup

---
+++ README.md
@@ -0,0 +1,9 @@
+## Backend Project Practice
+A modern industry-level backend project setup:
+
+1. Express.js
+2. TypeScript
+3. Modular Architecture
+4. Reusable Response Handler
+5. Zod Validation
+6. Middleware Pattern
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import cors from "cors";
+import cookieParse from "cookie-parser";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParse());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    status: "Ok",
+    message: "Server is running",
+    uptime: process.uptime,
+  });
+});
+
